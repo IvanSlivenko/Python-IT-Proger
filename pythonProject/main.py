e5bfe2a8e1fae80354eb3df5586e450e307eb172
IvanSlivenko <ivan8822@ulr.net>
2023-12-19 17:02:21 +0200

    19-12-2023-17-02

---
+++ pythonProject/main.py
@@ -0,0 +1,22 @@
+# виведемо перше повідомлення
+print("Результат : 5")
+print("Результат : ", 7  )
+print("Результат : ", 7, 8, "."  )
+
+
+print("Результат : ", 7, 8, ".",sep="/")
+print("Результат : ", 7, 8, ".",sep="")
+
+print("Результат : ", 7, 10, ".",sep="",end="\n")
+
+print("Результат : ", 7, 10,sep="",end="!")
+print("Second Line")
+print('Second  " Line')
+print("Second \" Line")
+print("Second \" L\ni\nn\ne")
+print("Second \" \t Line")
+print("Second \"  \\  \t Line")
+
+print("Результат:", 5+5 )
+print("Результат:", 5 ** 3 )
+print("Результат:", 5 // 2 )
