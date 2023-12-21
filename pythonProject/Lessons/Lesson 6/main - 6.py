0ad40901bf91448937551e92b8b8330753c02b1b
IvanSlivenko <ivan8822@ulr.net>
2023-12-21 17:00:23 +0200

    21-12-2023-17-00

---
+++ pythonProject/Lessons/Lesson 6/main - 6.py
@@ -0,0 +1,74 @@
+
+#-----------------------------
+# for i in range(7):
+#     if i > 0:
+#        print(i)
+
+# ----------------------------
+# for i in range(2, 7):
+#     print(i)
+#
+# ----------------------------
+# for i in range(1, 20, 2):
+#     print(i)
+
+# ----------------------------
+# for i in "Hello world":
+#     print(i)
+# ----------------------------
+# word = "Hello world"
+#
+# for i in word:
+#     print(i)
+#
+# ----------------------------
+# word = "Hello world"
+#
+# for i in word:
+#     print(i*3)
+# # ----------------------------
+# count = 0
+# word = "Hello world !"
+#
+# for i in word:
+#     if i == "o":
+#         count += 1
+# print("count = ", str(count))
+#------------------------------
+# i = 5
+# while i <= 15:
+#     print(i)
+#     i += 2
+#-----------------------------
+# logOut = True
+# userPassword = 258
+# UserLogin = "ivan"
+#
+# while logOut:
+#     if (int(input("Enter password: ")) == userPassword
+#         and input("Enter login: ") == UserLogin):
+#             logOut = False
+#             print("you have successfully logged in")
+#     else:
+#         print("you are not logged in")
+
+#----------------------------------
+# for i in range(1, 11):
+#     if i >= 10:
+#         break
+#
+#     if i %  2 == 0:
+#         continue
+#     print(i)
+#-------------------------------------
+found= None
+for i in "hello":
+
+    if i == "l":
+        found = True
+        break
+else:
+    found = False
+print(found)
+
+
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,9 +1,39 @@
+#----------------------------------------------
+# nums = [5,7,2,5,4,5,6,True,"text", 6.5, [5,7]]
+# print(nums)
+# nums[0]=50
+# nums[7]= "seven"
+# print(nums)
+#-----------------------------------------------
+# nums = [5,7,2,5,4,5,6,True,"text", 6.5, [5,7]]
 
-#-----------------------------
-# for i in range(7):
-#     if i > 0:
-#        print(i)
+#-------------  last element --------------------
+# print(nums[-1])
+# print(nums[-1][1])
+#------------------------------------------------
+numbers = [5,2,7]
+print("numbers",numbers)
+numbers.append(100)
+print("numbers.append(100)",numbers)
+numbers.insert(1,True)
+print("numbers.insert(1,True)",numbers)
+numbers.extend([5, 6, 7])
+print("numbers.extend([5, 6, 7])", numbers)
 
-# ----------------------------
-for i in range(2, 7):
-    print(i)
+b = [4, 5, 6]
+print("b",b)
+numbers.extend(b)
+print("numbers.extend(b)",numbers)
+numbers.sort()
+print("numbers.sort()",numbers)
+numbers.reverse()
+print("numbers.reverse()",numbers)
+numbers.pop()
+print("numbers.pop()",numbers)
+numbers.pop(0)
+print("numbers.pop(0)",numbers)
+numbers.pop(-2)
+print("numbers.pop(-2)",numbers)
+numbers.remove(7)
+print("numbers.remove(7)", numbers)
+
